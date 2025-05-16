3d0686dfa20e1201cded8408e514db9e237c8324
formationWeb-Lus <jiresselusa127@gmail.com>
2025-05-16 06:55:17 -0700

    project of week 2

--- controllers/users.js
+++ controllers/users.js
@@ -3,27 +3,79 @@ const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-  try {
-    const result = await mongodb.getDatabase().collection('users').find(); // ✅ "users" au pluriel
-    const users = await result.toArray();
+  const result = await mongodb.getDatabase().collection('users').find();
+  result.toArray().then((users) => {
+    res.setHeader('Content-Type', 'application/json');
     res.status(200).json(users);
-  } catch (err) {
-    res.status(500).json({ message: 'Erreur serveur', error: err });
-  }
+  });
 };
 
 const getSingle = async (req, res) => {
-  try {
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().collection('users').find({ _id: userId });
-    const users = await result.toArray();
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb.getDatabase().collection('users').find({ _id: userId });
+  result.toArray().then((users) => {
+    res.setHeader('Content-Type', 'application/json');
     res.status(200).json(users[0]);
+  });
+};
+
+const createUser = async (req, res) => {
+  const user = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+
+  try {
+    const response = await mongodb.getDatabase().collection('users').insertOne(user);
+    res.status(201).json({ id: response.insertedId });
   } catch (err) {
-    res.status(500).json({ message: 'Erreur serveur', error: err });
+    res.status(500).json({ message: 'Erreur lors de la création', error: err });
+  }
+};
+
+const updateUser = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const user = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+
+  try {
+    const response = await mongodb.getDatabase().collection('users').replaceOne({ _id: userId }, user);
+    if (response.modifiedCount > 0) {
+      res.status(204).send(); // Pas de contenu, succès
+    } else {
+      res.status(404).json({ message: 'Utilisateur non trouvé ou inchangé' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Erreur lors de la mise à jour', error: err });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  try {
+    const response = await mongodb.getDatabase().collection('users').deleteOne({ _id: userId });
+    if (response.deletedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(404).json({ message: 'Utilisateur non trouvé' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Erreur lors de la suppression', error: err });
   }
 };
 
 module.exports = {
   getAll,
-  getSingle
+  getSingle,
+  createUser,
+  updateUser,
+  deleteUser
 };
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
-
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/users');
 
-router.get('/', usersController.getAll);          // /users
-router.get('/:id', usersController.getSingle);    // /users/:id
+router.get('/', usersController.getAll);          
+router.get('/:id', usersController.getSingle);    
+router.post('/', usersController.createUser);     // ✅
+router.put('/:id', usersController.updateUser);   // ✅
+router.delete('/:id', usersController.deleteUser); // ✅
 
 module.exports = router;
